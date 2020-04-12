8b034242e9429d7fb95d86b721a317229acc1055
dinith72 <jdinith72@gmail.com>
2020-04-12 17:00:04 +0530

    changing the font style of the animated text

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -61,7 +61,7 @@ const Portfolio = function() {
 		
 			Typed.new("#writing-text", {
 				strings: [
-					"am a Full-Stack Web Developer.", "love to code.", "create impact through developing.", "solve problems."
+					"am a Full-Stack Web Developer.", "love to code.", "creates an impact.", "solve problems."
 				],
 				// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 				stringsElement: null,
