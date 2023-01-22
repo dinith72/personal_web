b80fe060ee464c0447e8e6baa539299851e0a9bb
Dinith Jayabodhi <Dinith.Jayabodhi@cube.global>
2023-01-22 08:18:05 +0530

    added the typewiriter effect

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,24 +2,26 @@
 import React from "react";
 import { Col, Container, Row, Button } from "react-bootstrap";
 import imgPotrait from "../assets/dinith.png";
+import Typewriter from "typewriter-effect"
 
 import "./HomePage.scss";
 
 export function HomePage(props) {
   const TEXTS = [
-    "Frontend Engineer",
-    "Backend Engineer",
+    
+    "Fullstack Developer",
+    "DevOps Engineer",
     "Cloud Engineer",
     "Blockchain  Enthusiast",
   ];
-  const [index, setIndex] = React.useState(0);
+  // const [index, setIndex] = React.useState(0);
 
-  React.useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIndex((index) => index + 1);
-    }, 4000);
-    return () => clearTimeout(intervalId);
-  }, []);
+  // React.useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setIndex((index) => index + 1);
+  //   }, 4000);
+  //   return () => clearTimeout(intervalId);
+  // }, []);
 
   return (
     <Container id="homePg">
@@ -30,7 +32,13 @@ export function HomePage(props) {
           <p> I am </p>
         </Row>
         <Row className="text-row">
-          <p className="jobs"> {TEXTS[index % TEXTS.length]} </p>
+          {/* <p className="jobs"> {TEXTS[index % TEXTS.length]} </p> */}
+          <Typewriter options={{
+            strings: TEXTS,
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+          }} />
         </Row>
         <Row className="text-row">
          <Button id="resume" variant="outline-success" size="lg">Download Resume</Button>
