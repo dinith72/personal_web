88ee6fb786c97325c0d986d1460a9264e3e02761
dinith72 <jdinith72@gmail.com>
2023-02-26 11:15:23 +0530

    added stack overflow info card

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import { Col, Container,  } from "react-bootstrap";
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 // import MyProjects from './MyProjects';
-import Bio from './Bio';
+import Bio from "./Bio";
+import Cards from "./cards";
+import "./About.scss";
 
 export function About(props) {
- 
-   
-    return (
-      <Container id="about">
-        <Col lg="6" md="12" className="animeTxt">
-        </Col>
-        <Col lg="6" md="12" className="animeTxt">
+  return (
+    <Container id="about">
+      <Row>
+       
+        <Col lg="6" md="6" className="bio">
           <Bio></Bio>
         </Col>
-      </Container>
-    );
-  }
-  export default About;
+        <Col lg="6" md="6" className="cards">
+          <Cards />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+export default About;
