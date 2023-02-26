4a0a8e99775fdbc6791c961843e958da5e4d42df
dinith72 <jdinith72@gmail.com>
2023-02-26 13:47:21 +0530

    added axios to fetch data  from meta api

--- src/components/about/socard.js
+++ src/components/about/socard.js
@@ -1,49 +1,69 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import soImage from "../../assets/stack-overflow.png";
+import axios from 'axios';
 import "./card.scss";
 
+const baseURL = 'https://api.stackexchange.com//2.3/users/8313114?order=asc&sort=reputation&site=stackoverflow&filter=!9aV0efSK4'
 export function SoCard(prop) {
-  const userData = {
-    link: "https://stackoverflow.com/users/8313114/dinith-jayabodhi",
-    display_name: "dinith jayabodhi",
-    reputation: 200,
-    badge_counts: {
-      bronze: 19,
-      silver: 8,
-      gold: 2,
-    },
-  };
+
+  const [soData, setSoData] = React.useState(null);
+
+  React.useEffect(() => {
+    const config = {
+      headers:{
+        Accept: 'text/plain'
+      }
+    };
+    axios
+    .get(baseURL,config)
+    .then((response) => {
+      setSoData(response.data.items[0]);
+    });
+  }, []);
+  // const soData = {
+  //   link: "https://stackoverflow.com/users/8313114/dinith-jayabodhi",
+  //   display_name: "dinith jayabodhi",
+  //   reputation: 200,
+  //   badge_counts: {
+  //     bronze: 19,
+  //     silver: 8,
+  //     gold: 2,
+  //   },
+  // };
+  if (!soData) 
+    return null;
 
   return (
+   
     <Container>
-      <a href={userData.link}>
+      <a href={soData.link}>
         <div class="so-card">
           <div class="so-logo">
             <img src={soImage} alt=""></img>
           </div>
           <div class="so-header">
-            <h4 class="so-display-name">{userData.display_name}</h4>
+            <h4 class="so-display-name">{soData.display_name}</h4>
           </div>
           <div class="so-content">
-            <p class="so-reputation">{userData.reputation}</p>
+            <p class="so-reputation">{soData.reputation}</p>
             <small class="so-reputation-label">Stackoverflow reputation</small>
           </div>
 
           <div class="so-footer">
             <span class="so-badge-wrapper">
               <span class="so-badge so-badge-gold"></span>
-              <span class="so-badge-count">{userData.badge_counts.gold}</span>
+              <span class="so-badge-count">{soData.badge_counts.gold}</span>
             </span>
 
             <span class="so-badge-wrapper">
               <span class="so-badge so-badge-silver"></span>
-              <span class="so-badge-count">{userData.badge_counts.silver}</span>
+              <span class="so-badge-count">{soData.badge_counts.silver}</span>
             </span>
 
             <span class="so-badge-wrapper">
               <span class="so-badge so-badge-bronze"></span>
-              <span class="so-badge-count">{userData.badge_counts.bronze}</span>
+              <span class="so-badge-count">{soData.badge_counts.bronze}</span>
             </span>
           </div>
         </div>
